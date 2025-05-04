2a84096e92f9c0c0f45e284be479b935ccbcd15d
Dionathan Boing Mesquita <dionathandev@gmail.com>
2025-05-04 00:39:22 -0300

    Ajustes while

--- exerc4/Program.cs
+++ exerc4/Program.cs
@@ -1,38 +1,55 @@
-﻿using System;
-using System.Text.RegularExpressions;
-class Program {
+﻿class Program
+{
+        static void Main()
+    {
 
-    static void Main (){
+        Boolean menu = true;
 
-        Console.WriteLine("Digite uma opção: ");
-        Console.WriteLine("1 - Celsius para Fahrenheit");
-        Console.WriteLine("2 - Fahrenheit para Celsius");
-        Console.WriteLine("3 - Sair");
-        int num = int.Parse(Console.ReadLine() ?? "0");
+        while (menu == true)
+        {
 
-        switch (num) {
-            case 1:{
-                Console.WriteLine("Digite a temperatura em celsius: ");
-                float celsius = float.Parse(Console.ReadLine() ?? "0");
-                float fahrenheit = (celsius * 1.8f) + 32;
-                Console.WriteLine("A temperatura em Fahrenheit é " + fahrenheit);
-                break;
+
+            Console.WriteLine("[1] Celsius para Fahrenreit");
+            Console.WriteLine("[2] Fahrenreit para Celsius");
+            Console.WriteLine("[3] Sair \n ");
+            Console.WriteLine("Digite a opção desejada: ");
+            int opcao = int.Parse( Console.ReadLine() ?? "0");
+
+
+            switch (opcao)
+            {
+                case 1:
+
+                    {
+                        Console.WriteLine("Digite a temperatura em celsius: ");
+                        float celsius = float.Parse(Console.ReadLine() ?? "0");
+                        float fahrenheit = (celsius * 1.8f) + 32;
+                        Console.WriteLine("A temperatura em Fahrenheit é " + fahrenheit + "\n");
+                        break;
+                    }
+
+                case 2:
+
+                    {
+                        Console.WriteLine("Digite a temperatura em Fahrenheit: ");
+                        float fahrenheit = float.Parse(Console.ReadLine() ?? "0");
+                        float celsius = (fahrenheit - 32) / 1.8f;
+                        Console.WriteLine("A temperatura em Celsius é " + celsius + "\n");
+                        break;
+                    }
+
+                case 3:
+                    Console.WriteLine("Saindo...");
+                    return;
+
+                default:
+                    Console.WriteLine("Opção inválida. \n");
+                    break;
             }
-            case 2:{
-                Console.WriteLine("Digite a temperatura em Fahrenheit: ");
-                float fahrenheit = float.Parse(Console.ReadLine() ?? "0");
-                float celsius = (fahrenheit - 32) / 1.8f;
-                Console.WriteLine("A temperatura em Celsius é " + celsius);
-                break;
-            }
-            case 3:
-            Console.WriteLine("Saindo...");
-                Environment.Exit(0);
-                break;
 
-            default:
-                Console.WriteLine("Opção inválida.");
-                break;
         }
+
+
     }
+
 }
