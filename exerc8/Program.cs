82c30a139264b1b87a76f29d554ea3f20e635cdc
Dionathan Boing Mesquita <dionathandev@gmail.com>
2025-05-05 19:26:30 -0300

    Ajuste formatação da entrada

--- exerc8/Program.cs
+++ exerc8/Program.cs
@@ -1,22 +1,32 @@
-﻿using System;
-class Program {
-    static void Main() {
-       
+using System;
+using System.Globalization;
+class Program
+{
+    static void Main()
+    {
+
         Console.WriteLine("Digite a sua altura: ");
-        float altura = float.Parse(Console.ReadLine() ?? "0");
+        float altura = float.Parse(Console.ReadLine() ?? "0", CultureInfo.InvariantCulture);
 
         Console.WriteLine("Digite o seu peso: ");
-        float peso = float.Parse(Console.ReadLine() ?? "0");
+        float peso = float.Parse(Console.ReadLine() ?? "0", CultureInfo.InvariantCulture);
 
         float imc = peso / (altura * altura);
 
-        if(imc < 18.5){
+        if (imc < 18.5)
+        {
             Console.WriteLine("Abaixo do peso.");
-        } else if(imc >= 18.5 && imc < 25){
+        }
+        else if (imc >= 18.5 && imc <= 24.9)
+        {
             Console.WriteLine("Peso normal.");
-        } else if(imc > 25 && imc < 30){
+        }
+        else if (imc >= 25 && imc <= 29.9)
+        {
             Console.WriteLine("Sobrepeso.");
-        } else if(imc >= 30){
+        }
+        else
+        { 
             Console.WriteLine("Obesidade.");
         }
 
