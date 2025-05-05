0e72286a63608413e581a24b05e96ce7d3cfa196
Dionathan Boing Mesquita <dionathandev@gmail.com>
2025-05-05 19:30:03 -0300

    Ajuste casas decimais

--- exerc8/Program.cs
+++ exerc8/Program.cs
@@ -1,34 +1,30 @@
 using System;
 using System.Globalization;
-class Program
-{
-    static void Main()
-    {
 
-        Console.WriteLine("Digite a sua altura: ");
-        float altura = float.Parse(Console.ReadLine() ?? "0", CultureInfo.InvariantCulture);
+class Program {
+    static void Main() {
+        Console.WriteLine("Digite a sua altura (ex: 1,74 ou 1.74): ");
+        string alturaInput = Console.ReadLine() ?? "0";
+        alturaInput = alturaInput.Replace(',', '.');
+        float altura = float.Parse(alturaInput, CultureInfo.InvariantCulture);
 
         Console.WriteLine("Digite o seu peso: ");
-        float peso = float.Parse(Console.ReadLine() ?? "0", CultureInfo.InvariantCulture);
+        string pesoInput = Console.ReadLine() ?? "0";
+        pesoInput = pesoInput.Replace(',', '.');
+        float peso = float.Parse(pesoInput, CultureInfo.InvariantCulture);
 
         float imc = peso / (altura * altura);
 
-        if (imc < 18.5)
-        {
+        Console.WriteLine($"Seu IMC é: {imc:F2}");
+
+        if(imc < 18.5){
             Console.WriteLine("Abaixo do peso.");
-        }
-        else if (imc >= 18.5 && imc <= 24.9)
-        {
+        } else if(imc >= 18.5 && imc <= 24.9){
             Console.WriteLine("Peso normal.");
-        }
-        else if (imc >= 25 && imc <= 29.9)
-        {
+        } else if(imc >= 25 && imc <= 29.9){
             Console.WriteLine("Sobrepeso.");
-        }
-        else
-        { 
+        } else {
             Console.WriteLine("Obesidade.");
         }
-
     }
 }
